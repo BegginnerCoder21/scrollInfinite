680e7991377ee22e2cc6fd970d278eaa219a243b
Teguera <aboubacarteguera21@gmail.com>
2023-07-29 11:52:57 +0200

    debut du projet scroll infinite

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Home from './components/views/Home.vue';
+
+
+</script>
+
+<template>
+    <main>
+      <Home />
+    </main>
+</template>
+
+<style scoped>
+
+</style>
